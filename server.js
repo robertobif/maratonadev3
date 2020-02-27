c433ef6c61aac15c248a6030f8fcda8eaa7d6863
robertobif <rbifdm@gmail.com>
2020-02-26 22:12:12 -0300

    versão 0.1

---
+++ server.js
@@ -0,0 +1,63 @@
+
+//configurando o servidor
+const express = require("express")
+const server = express()
+
+//configurar o servidor para apresentar arquivos extras
+server.use(express.static('public'))
+
+//habilitar body do formulario
+server.use(express.urlencoded({ extended: true }))
+
+//configurar a conexão com o banco de dados
+const Pool = require('pg').Pool
+const db = new Pool({
+    user: 'postgres',
+    password: '0000',
+    host: 'localhost',
+    port: 5432,
+    database: 'doe'
+})
+
+// configurando a template engine
+const nunjucks = require("nunjucks");
+nunjucks.configure("./", {
+    express: server,
+    noCache: true,
+})
+
+//configurar a apresentação da página
+
+server.get("/", function(req, res) {
+    db.query("SELECT * FROM donors", function(err, result) {
+        if (err) return res.send("Oops! ocorreu um erro no banco de dados!");
+        const donors = result.rows;
+        res.render('index.html', { donors });
+    })
+
+});
+
+server.post("/", function (req, res) {
+    const name = req.body.name;
+    const email = req.body.email;
+    const blood = req.body.blood;
+
+    if (name === '' || email === '' || blood === '') {
+        return res.send("Todos os campos são obrigátorios.")
+    }
+
+    const query = `INSERT INTO donors ("name", "email", "blood")
+    VALUES ($1, $2, $3)`;
+    const values = [name, email, blood];
+    db.query(query, values, function (err) {
+        if (err) return res.send("Erro no banco de dados.");
+        return res.redirect("/");
+    })
+});
+
+
+
+//ligar o servidor e permitir o acesso na porta 3000
+server.listen(3000, function () {
+    console.log("Iniciei o servidor")
+})
